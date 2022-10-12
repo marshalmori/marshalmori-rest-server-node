12767d316fdbdb712d5cc78496596e8839bf52a0
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-10-11 21:30:39 -0300

    Cambiar visualmente _id por uid em Mongoose

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,47 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login ok",
-  });
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/generar-jwt");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario e/ou senha não estão corretos - correo",
+      });
+    }
+
+    // Si el usuario está activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario e/ou senha não estão corretos - estado: false",
+      });
+    }
+    // Verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario e/ou senha não estão corretos - password",
+      });
+    }
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Fale com o administrador do sistema.",
+    });
+  }
 };
 
 module.exports = { login };
