a284981a74d320cc684172649238752c59075ec8
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-10-17 20:01:34 -0300

    Login e Logout Google Identity

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -44,4 +45,46 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      const data = {
+        nombre,
+        correo,
+        password: ":P",
+        img,
+        google: true,
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    //Se o usuário está cadastrado e está com estado: false
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Por favor entre em contato com o administrador.",
+      });
+    }
+
+    // Gerar o JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Não foi possível verificar o token.",
+    });
+  }
+};
+
+module.exports = { login, googleSignIn };
